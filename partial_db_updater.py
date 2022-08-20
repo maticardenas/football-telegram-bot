46b31632105750584c021192978373fada7a024f
Matias Cardenas <cardenasmatias.1990@gmail.com>
2022-08-20 16:14:00 +0200

    chore: changing updater frequency and range

--- partial_db_updater.py
+++ partial_db_updater.py
@@ -40,7 +40,7 @@ def get_fixture_update_lots(
 
 
 def get_all_fixtures_ids_to_update() -> List["DBFixture"]:
-    todays_fixtures = FIXTURES_DB_MANAGER.get_games_in_surrounding_n_hours(2)
+    todays_fixtures = FIXTURES_DB_MANAGER.get_games_in_surrounding_n_hours(3)
 
     return [fixture.id for fixture in todays_fixtures if fixture]
 
