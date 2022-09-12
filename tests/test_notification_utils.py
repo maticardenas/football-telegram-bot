cefe9a3e7ebd12ff549dfbea464e87f6d5194213
Matias Cardenas <cardenasmatias.1990@gmail.com>
2022-09-13 00:08:32 +0200

    fix: fixing tests after changes in notifications

--- tests/test_notification_utils.py
+++ tests/test_notification_utils.py
@@ -18,7 +18,7 @@ def test_telegram_next_team_or_league_fixture_notification(
     assert telegram_next_team_or_league_fixture_notification(
         fixture, "River Plate"
     ) == (
-        "👋Hi ! \n\nThe next match of River Plate is on 🗓 01-01-2019\n\n\n🇪🇺 <strong>16:00 HS </strong>\n🇦🇷 <strong>12:00 HS</strong>\n\n⏰  left for the game.\n\n⚽ <strong>River Plate vs. Boca Juniors</strong>\n🏆 <strong>Copa de la Superliga (ARG)</strong>\n🏟 <strong>Estadio Monumental</strong>\n👮\u200d♀️ <strong>Perluigi Colina</strong>\n\n",
+        "👋Hi ! \n\nThe next match of River Plate is on 🗓 Tue. 01-01-2019\n\n\n🇪🇺 <strong>16:00 HS </strong>\n🇦🇷 <strong>12:00 HS</strong>\n\n⏰  left for the game.\n\n⚽ <strong>River Plate vs. Boca Juniors</strong>\n🏆 <strong>Copa de la Superliga (ARG)</strong>\n🏟 <strong>Estadio Monumental</strong>\n👮\u200d♀️ <strong>Perluigi Colina</strong>\n\n",
         "image.jpg",
     )
 
@@ -34,6 +34,6 @@ def test_telegram_last_team_or_league_fixture_notification(
     assert telegram_last_team_or_league_fixture_notification(
         fixture, "River Plate"
     ) == (
-        "👋Hi !\n\nThe last match of River Plate was on 🗓 01-01-2019\n \n\n😢 <strong>Started</strong>\n\n<strong>⚽ River Plate vs. Boca Juniors</strong>\n🏆 <strong>Copa de la Superliga (ARG)</strong>\n📌 <strong>Primera Fecha</strong>\n🏟 <strong>Estadio Monumental</strong>\n👮\u200d♀️ <strong>Perluigi Colina</strong>\n\n📽 <a href='https://www.youtube.com/results?search_query=River Plate+vs+Boca Juniors+jugadas+resumen'>HIGHLIGHTS</a>",
+        "👋Hi !\n\nThe last match of River Plate was on 🗓 Tue. 01-01-2019\n \n\n😢 <strong>Started</strong>\n\n<strong>⚽ River Plate vs. Boca Juniors</strong>\n🏆 <strong>Copa de la Superliga (ARG)</strong>\n📌 <strong>Primera Fecha</strong>\n🏟 <strong>Estadio Monumental</strong>\n👮\u200d♀️ <strong>Perluigi Colina</strong>\n\n📽 <a href='https://www.youtube.com/results?search_query=River Plate+vs+Boca Juniors+jugadas+resumen'>HIGHLIGHTS</a>",
         "image.jpg",
     )
