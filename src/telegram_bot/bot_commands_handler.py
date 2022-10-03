0c835a5a2f8c5afcc1edd55910e1ac0f584e6190
Matias Cardenas <cardenasmatias.1990@gmail.com>
2022-10-03 22:02:27 +0200

    chore: adding logs for debugging

--- src/telegram_bot/bot_commands_handler.py
+++ src/telegram_bot/bot_commands_handler.py
@@ -97,6 +97,7 @@ class NotifierBotCommandsHandler:
                 current_fitting_fixtures = []
             else:
                 fixtures_text += "\n\n" + fixture_text
+                logger.info(f"Appended fixture text -> {fixtures_text}")
                 current_fitting_fixtures.append(fixture)
 
         if current_fitting_fixtures:
