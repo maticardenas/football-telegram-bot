98556ef7c8be8368e428bee20debd69763dd940b
Matias Cardenas <cardenasmatias.1990@gmail.com>
2023-02-13 00:44:45 +0100

    feat: adding conversation handling for search commands, also for adding favourite leagues

--- src/telegram_bot/fav_teams_and_leagues_commands.py
+++ src/telegram_bot/fav_teams_and_leagues_commands.py
@@ -1,8 +1,17 @@
-from telegram import Update
+from typing import List
+
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update
 from telegram.ext import ConversationHandler
 
+from src.db.notif_sql_models import FavouriteTeam, Team
 from src.emojis import Emojis
-from src.notifier_constants import ADD_FAVOURITE_TEAM
+from src.notifier_constants import (
+    ADD_FAVOURITE_LEAGUE,
+    ADD_FAVOURITE_TEAM,
+    SEARCH_LEAGUE,
+    SEARCH_LEAGUES_BY_COUNTRY,
+    SEARCH_TEAM,
+)
 from src.notifier_logger import get_logger
 from src.telegram_bot.bot_commands_handler import (
     FavouriteLeaguesCommandHandler,
@@ -64,7 +73,7 @@ async def favourite_teams(update: Update, context):
             chat_id=update.effective_chat.id, text=validated_input, parse_mode="HTML"
         )
     else:
-        text = commands_handler.get_favourite_teams()
+        text = commands_handler.get_favourite_teams_response()
 
         await context.bot.send_message(
             chat_id=update.effective_chat.id, text=text, parse_mode="HTML"
@@ -79,26 +88,147 @@ async def delete_favourite_team(update: Update, context):
         context.args, update.effective_user.first_name, str(update.effective_chat.id)
     )
 
-    validated_input = commands_handler.validate_command_input()
-
-    if validated_input:
-        await context.bot.send_message(
-            chat_id=update.effective_chat.id, text=validated_input, parse_mode="HTML"
+    if not len(context.args):
+        await own_favourite_teams_inline_keyboard(
+            update, context, commands_handler.get_favourite_teams()
         )
     else:
-        text = commands_handler.delete_favourite_team()
+        validated_input = commands_handler.validate_command_input()
 
-        await context.bot.send_message(
-            chat_id=update.effective_chat.id, text=text, parse_mode="HTML"
-        )
+        if validated_input:
+            await context.bot.send_message(
+                chat_id=update.effective_chat.id,
+                text=validated_input,
+                parse_mode="HTML",
+            )
+        else:
+            text = commands_handler.delete_favourite_team()
+
+            await context.bot.send_message(
+                chat_id=update.effective_chat.id, text=text, parse_mode="HTML"
+            )
+
+
+async def delete_favourite_team_callback_handler(update: Update, context) -> None:
+    """Parses the CallbackQuery and updates the message text."""
+    query = update.callback_query
+
+    await query.answer()
+
+    context.args = [query.data.split()[1]]
+
+    await delete_favourite_team(update, context)
+
+
+async def own_favourite_teams_inline_keyboard(
+    update: Update, context, teams: List[Team]
+):
+    keyboard = (
+        [
+            InlineKeyboardButton(
+                teams[i].name, callback_data=f"delete_favourite_team {teams[i].id}"
+            ),
+            InlineKeyboardButton(
+                teams[i + 1].name,
+                callback_data=f"delete_favourite_team {teams[i+1].id}",
+            ),
+        ]
+        for i in range(0, len(teams), 2)
+        if i + 1 < len(teams)
+    )
+
+    if len(teams) % 2 != 0:
+        keyboard = [
+            *keyboard,
+            [
+                InlineKeyboardButton(
+                    teams[-1].name,
+                    callback_data=f"delete_favourite_team {teams[-1].id}",
+                )
+            ],
+        ]
+
+    reply_markup = InlineKeyboardMarkup(keyboard)
+
+    text = f"Please choose the team you would like to remove from your favourites:"
+
+    await context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text=text,
+        reply_markup=reply_markup,
+        parse_mode="HTML",
+    )
+
+
+async def delete_favourite_league_callback_handler(update: Update, context) -> None:
+    """Parses the CallbackQuery and updates the message text."""
+    query = update.callback_query
+
+    await query.answer()
+
+    context.args = [query.data.split()[1]]
+
+    await delete_favourite_league(update, context)
+
+
+async def own_favourite_leagues_inline_keyboard(
+    update: Update, context, leagues: List[Team]
+):
+    keyboard = (
+        [
+            InlineKeyboardButton(
+                leagues[i].name,
+                callback_data=f"delete_favourite_league {leagues[i].id}",
+            ),
+            InlineKeyboardButton(
+                leagues[i + 1].name,
+                callback_data=f"delete_favourite_league {leagues[i+1].id}",
+            ),
+        ]
+        for i in range(0, len(leagues), 2)
+        if i + 1 < len(leagues)
+    )
+
+    if len(leagues) % 2 != 0:
+        keyboard = [
+            *keyboard,
+            [
+                InlineKeyboardButton(
+                    leagues[-1].name,
+                    callback_data=f"delete_favourite_league {leagues[-1].id}",
+                )
+            ],
+        ]
+
+    reply_markup = InlineKeyboardMarkup(keyboard)
+
+    text = f"Please choose the league you would like to remove from your favourites:"
+
+    await context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text=text,
+        reply_markup=reply_markup,
+        parse_mode="HTML",
+    )
 
 
 async def add_favourite_league(update: Update, context):
     logger.info(
         f"'add_favourite_league' command executed - by {update.effective_user.name}"
     )
+
+    await update.message.reply_text(
+        "Please insert the id of the league you would like me to add as your favourite.",
+    )
+
+    return ADD_FAVOURITE_LEAGUE
+
+
+async def add_favourite_league_handler(update: Update, context):
     commands_handler = FavouriteLeaguesCommandHandler(
-        context.args, update.effective_user.first_name, str(update.effective_chat.id)
+        [update.message.text],
+        update.effective_user.first_name,
+        str(update.effective_chat.id),
     )
 
     validated_input = commands_handler.validate_command_input()
@@ -123,18 +253,25 @@ async def delete_favourite_league(update: Update, context):
         context.args, update.effective_user.first_name, str(update.effective_chat.id)
     )
 
-    validated_input = commands_handler.validate_command_input()
-
-    if validated_input:
-        await context.bot.send_message(
-            chat_id=update.effective_chat.id, text=validated_input, parse_mode="HTML"
+    if not len(context.args):
+        await own_favourite_leagues_inline_keyboard(
+            update, context, commands_handler.get_favourite_leagues_response()
         )
     else:
-        text = commands_handler.delete_favourite_league()
+        validated_input = commands_handler.validate_command_input()
 
-        await context.bot.send_message(
-            chat_id=update.effective_chat.id, text=text, parse_mode="HTML"
-        )
+        if validated_input:
+            await context.bot.send_message(
+                chat_id=update.effective_chat.id,
+                text=validated_input,
+                parse_mode="HTML",
+            )
+        else:
+            text = commands_handler.delete_favourite_league()
+
+            await context.bot.send_message(
+                chat_id=update.effective_chat.id, text=text, parse_mode="HTML"
+            )
 
 
 async def favourite_leagues(update: Update, context):
@@ -155,7 +292,7 @@ async def favourite_leagues(update: Update, context):
             chat_id=update.effective_chat.id, text=validated_input, parse_mode="HTML"
         )
     else:
-        text = commands_handler.get_favourite_leagues()
+        text = commands_handler.get_favourite_leagues_response()
 
         await context.bot.send_message(
             chat_id=update.effective_chat.id, text=text, parse_mode="HTML"
@@ -185,8 +322,17 @@ async def search_team(update: Update, context):
     logger.info(
         f"'search_team {' '.join(context.args)}' command executed - by {update.effective_user.name}"
     )
+
+    await update.message.reply_text(
+        "Please insert the name (or part of the name) of the team you would like to search:",
+    )
+
+    return SEARCH_TEAM
+
+
+async def search_team_handler(update, context):
     command_handler = SearchCommandHandler(
-        context.args, update.effective_user.first_name
+        [update.message.text], update.effective_user.first_name
     )
     validated_input = command_handler.validate_command_input()
 
@@ -206,8 +352,17 @@ async def search_league(update: Update, context):
     logger.info(
         f"'search_league {' '.join(context.args)}' command executed - by {update.effective_user.name}"
     )
+
+    await update.message.reply_text(
+        "Please insert the name (or part of the name) of the league you would like to search:",
+    )
+
+    return SEARCH_LEAGUE
+
+
+async def search_league_handler(update, context):
     command_handler = SearchCommandHandler(
-        context.args, update.effective_user.first_name
+        [update.message.text], update.effective_user.first_name
     )
     validated_input = command_handler.validate_command_input()
 
@@ -227,8 +382,17 @@ async def search_leagues_by_country(update: Update, context):
     logger.info(
         f"'search_leagues_by_country {' '.join(context.args)}' command executed - by {update.effective_user.name}"
     )
+
+    await update.message.reply_text(
+        "Please insert the name (or part of the name) of the of the country you would like to search leagues for:",
+    )
+
+    return SEARCH_LEAGUES_BY_COUNTRY
+
+
+async def search_leagues_by_country_handler(update: Update, context):
     command_handler = SearchCommandHandler(
-        context.args, update.effective_user.first_name
+        [update.message.text], update.effective_user.first_name
     )
     validated_input = command_handler.validate_command_input()
 
--- src/telegram_bot/notifier_bot_handlers.py
+++ src/telegram_bot/notifier_bot_handlers.py
@@ -7,18 +7,31 @@ from telegram.ext import (
     filters,
 )
 
+from src.notifier_constants import (
+    ADD_FAVOURITE_LEAGUE,
+    ADD_FAVOURITE_TEAM,
+    SEARCH_LEAGUE,
+    SEARCH_LEAGUES_BY_COUNTRY,
+    SEARCH_TEAM,
+)
 from src.telegram_bot.fav_teams_and_leagues_commands import (
     add_favourite_league,
+    add_favourite_league_handler,
     add_favourite_team,
     add_favourite_team_handler,
     available_leagues,
     delete_favourite_league,
+    delete_favourite_league_callback_handler,
     delete_favourite_team,
+    delete_favourite_team_callback_handler,
     favourite_leagues,
     favourite_teams,
     search_league,
+    search_league_handler,
     search_leagues_by_country,
+    search_leagues_by_country_handler,
     search_team,
+    search_team_handler,
 )
 from src.telegram_bot.matches_commands import (
     last_match,
@@ -64,9 +77,6 @@ async def cancel(update, context) -> int:
 
 NOTIFIER_BOT_HANDLERS = [
     CommandHandler("start", start),
-    CommandHandler("search_team", search_team),
-    CommandHandler("search_league", search_league),
-    CommandHandler("search_leagues_by_country", search_leagues_by_country),
     CommandHandler("next_match", next_match),
     CommandHandler("upcoming_matches", upcoming_matches),
     CommandHandler("last_match", last_match),
@@ -85,7 +95,6 @@ NOTIFIER_BOT_HANDLERS = [
     CommandHandler("delete_time_zone", delete_time_zone),
     CommandHandler("favourite_teams", favourite_teams),
     CommandHandler("favourite_leagues", favourite_leagues),
-    CommandHandler("add_favourite_league", add_favourite_league),
     CommandHandler("delete_favourite_team", delete_favourite_team),
     CommandHandler("delete_favourite_league", delete_favourite_league),
     CommandHandler("notif_config", notif_config_inline_keyboard),
@@ -105,15 +114,63 @@ NOTIFIER_BOT_HANDLERS = [
         yesterday_matches_callback_handler, pattern="^.*yesterday_matches.*"
     ),
     CallbackQueryHandler(
+        delete_favourite_team_callback_handler, pattern="^.*delete_favourite_team.*"
+    ),
+    CallbackQueryHandler(
+        delete_favourite_league_callback_handler, pattern="^.*delete_favourite_league.*"
+    ),
+    CallbackQueryHandler(
         notif_config_callback_handler,
         pattern="^.*set_daily_notif_time|enable_notif_config|disable_notif_config.*",
     ),
     ConversationHandler(
         entry_points=[CommandHandler("add_favourite_team", add_favourite_team)],
         states={
-            4: [
+            ADD_FAVOURITE_TEAM: [
                 MessageHandler(
                     filters.TEXT & ~filters.COMMAND, add_favourite_team_handler
+                )
+            ]
+        },
+        fallbacks=[MessageHandler(filters.COMMAND, cancel)],
+    ),
+    ConversationHandler(
+        entry_points=[CommandHandler("add_favourite_league", add_favourite_league)],
+        states={
+            ADD_FAVOURITE_LEAGUE: [
+                MessageHandler(
+                    filters.TEXT & ~filters.COMMAND, add_favourite_league_handler
+                )
+            ]
+        },
+        fallbacks=[MessageHandler(filters.COMMAND, cancel)],
+    ),
+    ConversationHandler(
+        entry_points=[CommandHandler("search_team", search_team)],
+        states={
+            SEARCH_TEAM: [
+                MessageHandler(filters.TEXT & ~filters.COMMAND, search_team_handler)
+            ]
+        },
+        fallbacks=[MessageHandler(filters.COMMAND, cancel)],
+    ),
+    ConversationHandler(
+        entry_points=[CommandHandler("search_league", search_league)],
+        states={
+            SEARCH_LEAGUE: [
+                MessageHandler(filters.TEXT & ~filters.COMMAND, search_league_handler)
+            ]
+        },
+        fallbacks=[MessageHandler(filters.COMMAND, cancel)],
+    ),
+    ConversationHandler(
+        entry_points=[
+            CommandHandler("search_leagues_by_country", search_leagues_by_country)
+        ],
+        states={
+            SEARCH_LEAGUES_BY_COUNTRY: [
+                MessageHandler(
+                    filters.TEXT & ~filters.COMMAND, search_leagues_by_country_handler
                 )
             ]
         },
